21e66905cbf27dd9607ee6dc7ef338d409b09cf5
chastity <chastity@student.21-school.ru>
2023-11-18 12:49:02 +0300

    added credit and start deposit interfaces

--- src/SmartCalc4.0/src/main/java/edu/school21/SmartCal40/controllers/CreditController.java
+++ src/SmartCalc4.0/src/main/java/edu/school21/SmartCal40/controllers/CreditController.java
@@ -2,17 +2,32 @@ package edu.school21.SmartCal40.controllers;
 
 import edu.school21.SmartCal40.models.CreditCalcModel;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController // потом поправить
+@Controller
 @AllArgsConstructor
 public class CreditController {
-    final CreditCalcModel model;
+    final CreditCalcModel creditModel;
 
-    @PostMapping("/credit/{request}") // потом поправить
-    public String getMainPage(@PathVariable final String request) {
-        return null;
+    @GetMapping("/credit")
+    public String getCreditPage() {
+        return "credit";
+    }
+
+    @PostMapping("/credit") //  провалидировать все
+    public String getMainPage(
+            @RequestParam("summa") final String summa,
+            @RequestParam("percent") final String percent,
+            @RequestParam("period") final String period,
+            @RequestParam("term-type") final String termType,
+            @RequestParam("credit-type") final String creditType,
+            final Model model
+    ) {
+//        creditModel.calculate();
+        return "credit";
     }
 }
--- src/SmartCalc4.0/src/main/java/edu/school21/SmartCal40/controllers/DepositController.java
+++ src/SmartCalc4.0/src/main/java/edu/school21/SmartCal40/controllers/DepositController.java
@@ -2,17 +2,22 @@ package edu.school21.SmartCal40.controllers;
 
 import edu.school21.SmartCal40.models.DepositCalcModel;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController // потом поправить
+@Controller
 @AllArgsConstructor
 public class DepositController {
     final DepositCalcModel model;
 
-    @PostMapping("/deposit/{request}") // потом поправить
-    public String getMainPage(@PathVariable final String request) {
-        return null;
+    @GetMapping("/deposit")
+    public String getDepositPage() {
+        return "deposit";
+    }
+
+    @PostMapping("/deposit")
+    public String getMainPage() {
+        return "deposit";
     }
 }
